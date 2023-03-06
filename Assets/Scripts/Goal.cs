94db1153c7c399b8f08d97c91360ed144dd40ac5
Century178 <mrothenberger0918@gmail.com>
2023-03-05 18:34:17 -0700

    Working Goal

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Goal : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private int sceneIndex;
 
-    // Update is called once per frame
-    void Update()
+    void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(sceneIndex);
+        }
     }
 }
